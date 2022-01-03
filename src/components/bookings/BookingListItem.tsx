74ef275679d80ec97ff6d6d7a302b53bc3f21611
kowalakk <konradkowalak@gmail.com>
2022-01-03 15:39:54 +0100

    added bookings deletion

--- src/components/bookings/BookingListItem.tsx
+++ src/components/bookings/BookingListItem.tsx
@@ -5,6 +5,7 @@ import {ButtonType} from "common/enums/ButtonType";
 
 interface BookingListItemProps {
     booking: Booking
+    cancelCallback: (booking: Booking) => void
 }
 
 function BookingListItem(props: BookingListItemProps) {
@@ -14,9 +15,7 @@ function BookingListItem(props: BookingListItemProps) {
         navigate(`${process.env.PUBLIC_URL}/bookings/${props.booking.id}/details`);
     }
 
-    const handleCancelClick = () => {
-        
-    }
+    const handleCancelClick = props.cancelCallback;
 
     return (
         <div className="list-item">
@@ -34,11 +33,11 @@ function BookingListItem(props: BookingListItemProps) {
                     Details
                 </Button>
                 <Button
-                    click={() => handleCancelClick()}
+                    click={() => handleCancelClick(props.booking)}
                     type={ButtonType.ERROR}
                     icon="trash"
                 >
-                    Cancel
+                    Cancel booking
                 </Button>
             </div>
         </div>
