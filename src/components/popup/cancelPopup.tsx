a564b0e4dfc8df7c7c96097d038fc9da813a3692
kowalakk <konradkowalak@gmail.com>
2022-01-03 15:39:54 +0100

    added bookings deletion

---
+++ src/components/popup/cancelPopup.tsx
@@ -0,0 +1,84 @@
+import {Booking} from "../../common/types/Booking";
+import Popup from "./popup";
+import {ButtonType} from "../../common/enums/ButtonType";
+import Picture from "../utils/Picture";
+
+export interface cancelPopupInterface {
+    booking: Booking | null,
+    cancelBookingCallback: (booking: Booking) => void,
+    show: boolean,
+    close: (flag: boolean) => void,
+}
+
+const CancelPopup = (props: cancelPopupInterface) => {
+    const isCurrent = (dateString: string | undefined) => {
+        if(dateString === undefined)
+            return false;
+        const date = Date.parse(dateString);
+        return date <= Date.now()
+    }
+
+    const header = 'Cancel booking'
+    const title = isCurrent(props.booking?.checkOutDate) ? 'Confirm booking cancelation' : 'Can\'t cancel the booking';
+    const info = isCurrent(props.booking?.checkInDate) ? null : 'The renting currently takes place';
+    const buttons = isCurrent(props.booking?.checkOutDate)
+        ?
+        [{
+            text: 'Cancel booking',
+            props: {
+                type: ButtonType.POPUP_FN,
+                click: () => {
+                    if (props.booking !== null)
+                        props.cancelBookingCallback(props.booking);
+                }
+            }
+        },
+            {
+                text: 'Back',
+                props: {
+                    type: ButtonType.POPUP_FN,
+                    click: props.close
+                }
+            }]
+        :
+        [
+            {
+                text: 'Cancel booking',
+                props: {
+                    disabled: true,
+                    type: ButtonType.POPUP_FN,
+                }
+            },
+            {
+                text: 'Back',
+                props: {
+                    type: ButtonType.POPUP_FN,
+                    click: props.close
+                }
+            }
+        ];
+
+    return(
+        <>
+            {props.booking !== null &&
+                <Popup show={props.show} close={props.close} buttons={buttons} header={header}>
+                    <div className={'popup-title'}>
+                        <h1>{title}</h1>
+                        {info !== null && <p>{info}</p>}
+                    </div>
+                    <div className={'popup-info'}>
+                        <h3>Renter:</h3> <h2>{props.booking.renter.name}</h2>
+                        <h3>Flat:</h3> <h2>{props.booking.flat.name}</h2>
+                        <h3>Check in:</h3> <h2>{props.booking.checkInDate}</h2>
+                        <h3>Check out:</h3> <h2>{props.booking.checkOutDate}</h2>
+                    </div>
+                    <div className={'popup-picture'}>
+                        <Picture />
+                    </div>
+                </Popup>
+            }
+        </>
+    );
+}
+
+export default CancelPopup;
--- src/pages/Bookings/BookingsList.tsx
+++ src/pages/Bookings/BookingsList.tsx
@@ -3,14 +3,22 @@ import {Booking} from "common/types/Booking";
 import BookingListItem from "components/bookings/BookingListItem";
 import {useMount} from "react-use";
 import Skeleton from 'react-loading-skeleton';
+import {useState} from "react";
+import useNotification from "../../modules/useNotification";
+import BookingService from "../../services/BookingService";
+import CancelPopup from "../../components/popup/cancelPopup";
 
 function BookingsList() {
     const { bookingsLoading, bookings, fetchBookings } = useBookings();
+    const [showPopup, setShowPopup] = useState<boolean>(false);
+    const [toBeCanceled, setToBeCanceled] = useState<Booking | null>(null);
+    const { error, success } = useNotification();
 
     useMount(() => fetchBookings());
 
     const renderBookingsList = () => {
-        return bookings.map((booking: Booking) => <BookingListItem booking={booking} key={booking.id}/>);
+        return bookings.map((booking: Booking) =>
+            <BookingListItem booking={booking} key={booking.id} cancelCallback={triggerCancelBooking}/>);
     }
 
     const renderSkeleton = () => {
@@ -19,8 +27,21 @@ function BookingsList() {
         </div>)
     }
 
+    const triggerCancelBooking = (booking: Booking) => {
+        setToBeCanceled(booking);
+        setShowPopup(true);
+    }
+
+    const deleteBooking = (booking: Booking) => {
+        if (booking.id !== undefined) BookingService.destroy(booking.id)
+            .then(() => success({title: 'Delete operation succeeded', message: 'successfully deleted the booking'}))
+            .catch((e: any) => error({title: 'Delete operation failed', message: 'failed to delete the booking'}))
+            .finally(() => { setShowPopup(false); setToBeCanceled(null); fetchBookings(); });
+    }
+
     return (
         <div>
+            <CancelPopup show={showPopup} close={setShowPopup} booking={toBeCanceled} cancelBookingCallback={deleteBooking}/>
             {
                 bookingsLoading
                 ? renderSkeleton()
